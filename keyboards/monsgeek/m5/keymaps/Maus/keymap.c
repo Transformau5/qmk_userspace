99f5cf0e735a0808360e9f54c0872e4b3eb97fea
U-Maus\Kostek <Transformau5@gmail.com>
2025-04-18 19:51:05 +0200

    Set a gradient on all keys to the right of the main cluster, adjust the Num Lock and Caps Lock keys to turn blue for better visibility

--- keyboards/monsgeek/m5/keymaps/Maus/keymap.c
+++ keyboards/monsgeek/m5/keymaps/Maus/keymap.c
@@ -24,66 +24,75 @@ enum __layers {
     MAC_FN
 };
 
-bool rgb_matrix_indicators_user(void) {
-		//Change the hue of the numpad numbers
-		int numpad_matrix[3][3] = {{58, 59, 60}, {75, 76, 77}, {91, 92, 93}};
-		for (int j=0; j<3; j++){
-			for (int k=0; k<3; k++){
-				rgb_matrix_set_color(numpad_matrix[j][k], 0, 182, 79);
-			}
-		}
-		//Highlight the keys surrounding the numpad, except for Num Lock as this would prevent it from changing its hue depending on state
-		int numpad_border[11] = {16, 17, 18, 19, 38, 39, 40, 61, 94, 106, 107};
-		for (int l=0; l<11; l++){
-			rgb_matrix_set_color(numpad_border[l], 0, 182, 79);
-		}
-		//Set the colour of the arrow keys
-		int arrow_keys[4] = {90, 104, 103, 105};
-		for (int m=0; m<4; m++){
-			switch (m){
-				case 0:
-					rgb_matrix_set_color(arrow_keys[m], 135, 184, 0);
-					break;
-				case 1:
-					rgb_matrix_set_color(arrow_keys[m], 135, 184, 0);
-					break;
-				case 2:
-					rgb_matrix_set_color(arrow_keys[m], 184, 125, 0);
-					break;
-				case 3:
-					rgb_matrix_set_color(arrow_keys[m], 28, 184, 0);
-					break;
-			}
-		}
-		/*Set the gradient of the Home block (Insert, Home, Page Up, etc.)*/
-		int home_block[3][3] = {{13, 14, 15}, {34, 35, 36}, {55, 56, 57}};
-		for (int n=0; n<3; n++){
-			for (int o=0; o<3; o++){
-				if(o==0){
-					rgb_matrix_set_color(home_block[n][o], 184, 125, 0);
-				}
-				if(o==1){
-					rgb_matrix_set_color(home_block[n][o], 135, 184, 0);
-				}
-				if(o==2){
-					rgb_matrix_set_color(home_block[n][o], 28, 184, 0);
-				}
-			}
-		}
+void keyboard_post_init_user(void)
+{
+	rgb_matrix_enable_noeeprom();
+	rgb_matrix_set_speed_noeeprom(147);
+	rgb_matrix_sethsv_noeeprom(15, 255, 255);
+	rgb_matrix_mode_noeeprom(RGB_MATRIX_ALPHAS_MODS);
+}
 
+bool rgb_matrix_indicators_user(void) {
+	//Set the modifier keys to a certain colour because doing so the usual way is not a thing in userspaces it seems
+	int modifiers[] = {0, 11, 12, 20, 33, 41, 54, 74, 78, 89, 95, 96, 97, 99, 100, 101, 102};
+	for (int i=0; i<17; i++){
+		rgb_matrix_set_color(modifiers[i], 255, 21, 0);
+	}
+	//Set the gradient of the numpad keys except the Num Lock
+	int numpad_gradient_columns[4][6] = {{16, 58, 75, 91, 106, 106}, {17, 38, 59, 76, 92, 92}, {18, 39, 60, 77, 93, 107}, {19, 40, 61, 94, 94, 94}}; //Extremely ugly, repeating the last element of each sub-array just so that I can force a 2D array and call it a day
+	int numpad_gradient_colours[4][3] = {{255, 0, 200}, {255, 0, 255}, {200, 0, 255}, {144, 0, 255}};
+	for (int i=0; i<4; i++){
+		for (int j=0; j<6; j++){
+			rgb_matrix_set_color(numpad_gradient_columns[i][j], numpad_gradient_colours[i][0], numpad_gradient_colours[i][1], numpad_gradient_colours[i][2]);
+		}
+	}
+	//Set the colour of the arrow keys
+	int arrow_keys[4] = {90, 104, 103, 105};
+	for (int m=0; m<4; m++){
+		switch (m){
+			case 0:
+				rgb_matrix_set_color(arrow_keys[m], 255, 0, 89);
+				break;
+			case 1:
+				rgb_matrix_set_color(arrow_keys[m], 255, 0, 89);
+				break;
+			case 2:
+				rgb_matrix_set_color(arrow_keys[m], 255, 0, 34);
+				break;
+			case 3:
+				rgb_matrix_set_color(arrow_keys[m], 255, 0, 145);
+				break;
+		}
+	}
+	/*Set the gradient of the Home cluster (Insert, Home, Page Up, etc.)*/
+	int home_block[3][3] = {{13, 14, 15}, {34, 35, 36}, {55, 56, 57}};
+	for (int n=0; n<3; n++){
+		for (int o=0; o<3; o++){
+			if(o==0){
+				rgb_matrix_set_color(home_block[n][o], 255, 0, 34);
+			}
+			if(o==1){
+				rgb_matrix_set_color(home_block[n][o], 255, 0, 89);
+			}
+			if(o==2){
+				rgb_matrix_set_color(home_block[n][o], 255, 0, 145);
+			}
+		}
+	}
 	if (host_keyboard_led_state().caps_lock) {
-        rgb_matrix_set_color(62, 0, 254, 0); // Change the colour of Caps Lock upon activation
+        rgb_matrix_set_color(62, 0, 182, 79); // Change the colour of Caps Lock upon activation
     } else {
-        rgb_matrix_set_color(62, 0, 255, 0);
+        rgb_matrix_set_color(62, 255, 21, 0);
     }
 
 	if (host_keyboard_led_state().num_lock) {
-        rgb_matrix_set_color(37, 255, 0, 96); // Change the colour of Num Lock upon activation
+        rgb_matrix_set_color(37, 0, 182, 79); // Change the colour of Num Lock upon activation
     } else {
-        rgb_matrix_set_color(37, 0, 182, 79);
+        rgb_matrix_set_color(37, 255, 0, 200);
     }
 	return true;
 }
+
 
 
 // clang-format off
